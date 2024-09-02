567a8b0db52c4818c56cc121a5d045fc784c8af5
Hector Tellez <tellezhector@users.noreply.github.com>
2024-09-02 16:45:14 -0700

    Adds tests for consuming error time.

--- state_mutations_test.py
+++ state_mutations_test.py
@@ -8,15 +8,73 @@ import state_mutations
 class StateMutationsTest(unittest.TestCase):
     def test_increase_elapsed_time_if_running(self):
         init = state.load_state(
-            {'timer_state': state.TimerState.RUNNING, 'old_timestamp': 0}, 
-            now=1
+            {
+                'timer_state': state.TimerState.RUNNING,
+                'old_timestamp': 0,
+            },
+            now=1,
         )
         self.assertEqual(1.0, init.new_timestamp)
         self.assertEqual(0.0, init.old_timestamp)
 
         _, later = state_mutations.increase_elapsed_time_if_running().run(init)
-        self.assertEqual(state.TimerState.RUNNING, later.timer_state)
         self.assertEqual(1.0, later.elapsed_time)
+
+    def test_do_not_increase_elapsed_time_if_stopped(self):
+        init = state.load_state(
+            {
+                'timer_state': state.TimerState.STOPPED,
+                'old_timestamp': 0,
+            },
+            now=1,
+        )
+        self.assertEqual(1.0, init.new_timestamp)
+        self.assertEqual(0.0, init.old_timestamp)
+
+        _, later = state_mutations.increase_elapsed_time_if_running().run(init)
+        self.assertEqual(0.0, later.elapsed_time)
+
+    def test_do_not_increase_elapsed_time_if_paused(self):
+        init = state.load_state(
+            {
+                'timer_state': state.TimerState.PAUSED,
+                'old_timestamp': 0,
+            },
+            now=1,
+        )
+        self.assertEqual(1.0, init.new_timestamp)
+        self.assertEqual(0.0, init.old_timestamp)
+
+        _, later = state_mutations.increase_elapsed_time_if_running().run(init)
+        self.assertEqual(0.0, later.elapsed_time)
+
+    def test_consume_error_time_if_there_is_error_duration(self):
+        init = state.load_state(
+            {
+                'old_timestamp': 0,
+                'error_duration': 4,
+            },
+            now=1,
+        )
+        self.assertEqual(1.0, init.new_timestamp)
+        self.assertEqual(0.0, init.old_timestamp)
+        self.assertEqual(4.0, init.error_duration)
+
+        _, later = state_mutations.consume_error_time().run(init)
+        self.assertEqual(3.0, later.error_duration)
+
+    def test_do_not_consume_error_time_if_there_is_no_error_duration(self):
+        init = state.load_state(
+            {
+                'old_timestamp': 0,
+            },
+            now=1,
+        )
+        self.assertEqual(1.0, init.new_timestamp)
+        self.assertEqual(0.0, init.old_timestamp)
+
+        _, later = state_mutations.consume_error_time().run(init)
+        self.assertEqual(None, later.error_duration)
 
 
 if __name__ == '__main__':
