a4b0d0e27fc44d23dcff7307111011a596c06c60
Hector Tellez <tellezhector@users.noreply.github.com>
2024-09-08 15:13:04 -0700

    Mkes tests.py executable.

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
 import unittest
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
-    runner = unittest.TextTestRunner(verbosity=1, 
-                                     warnings='always')
-    runner.run(loader.discover(
-        start_dir='.',
-        pattern='*test.py',
-        ))
+    runner = unittest.TextTestRunner(verbosity=1, warnings='always')
+    runner.run(
+        loader.discover(
+            start_dir='.',
+            pattern='*test.py',
+        )
+    )
